d3c27aa1fdd2849287b7f44b66510d35a80816c0
Nattapat Sukcharoenpohn <Tlneclover@gmail.com>
2024-10-06 15:02:23 +0700

    header add pic

---
+++ src/app/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header',
+    //template: '<h1>Hello World</h1>'
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+  })
+  
+  export class HeaderComponent {
+   
+  
+  }
